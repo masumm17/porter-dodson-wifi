107937b3a4dbfdd729ada63462b13102bc095eb3
Mahbub <Mahbub@DESKTOP-PNVCMVR>
2018-11-05 20:18:50 +0600

    Clean config file

--- inc/pd-config.php
+++ inc/pd-config.php
@@ -2,25 +2,10 @@
 if (!defined('ABSPATH')) {
     die('-1');
 }
-/**
- * pdrudd
-6Mgs5c4*
-
-
-wsstage_pdwifi
-localhost:3306
-
-
-pdwff
-V3ft9$v5
-46.32.252.9
- */
 
 define('DB_TYPE', 'mysql');
 define('DB_NAME', 'wsstage_pdwifi');
-//define('DB_USER', 'root');
 define('DB_USER', 'pdrudd');
-//define('DB_PASWORD', 'mysql');
 define('DB_PASWORD', 'P6f6su0%');
 define('DB_SERVER', 'localhost');
 define('DB_SERVER_PORT', 3306);
